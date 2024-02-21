e045b305dde3814d62e39a2b924e2e05e966ba6c
Derpy113 <0210joma@gapp.uddevalla.se>
2024-02-21 13:49:00 +0100

    Added parameters to the rest of the UserDao functions (and some minor inconsequensial detail changes)

--- php/model/database/dao/UserDAO.php
+++ php/model/database/dao/UserDAO.php
@@ -9,18 +9,10 @@ class UserDAO
         $this->con = $con;
     }
 
-    public function countByName($username) //JOSEF TO-DO Add parameters here too
-    {
-        $sql = "select COUNT(*) as total from userprofile where Username = '$username'";
-        
-        $statement = $this->con->getPDO()->prepare($sql); 
-        $statement->execute();
-        return $statement->fetch(PDO::FETCH_ASSOC);
-    }
-
     public function getUserByUsername($username)
     {
-        $statement = $this->con->getPDO()->prepare('SELECT * FROM userprofile WHERE Username = :username');
+        $sqlQuery = 'SELECT * FROM userprofile WHERE Username = :username';
+        $statement = $this->con->getPDO()->prepare($sqlQuery);
         $statement->execute([ 'username' => $username ]);
         $statement->setFetchMode(PDO::FETCH_CLASS, 'userprofile');
         $user = $statement->fetch();
@@ -34,14 +26,22 @@ class UserDAO
         }
     }
 
-    public function createUser(string $Username, string $UserPassword) //JOSEF TO-DO Add parameters here too
+    public function countByName($username)
+    {
+        $sqlQuery = 'SELECT COUNT(*) AS total FROM userprofile WHERE Username = :username';
+        $statement = $this->con->getPDO()->prepare($sqlQuery); 
+        $statement->execute([ 'username' => $username ]);
+        return $statement->fetch(PDO::FETCH_ASSOC);
+        
+    }
+
+    public function createUser(string $username, string $userPassword)
     {
         //Note that the validation of data is done in CreateAccount, as that is the role of that controller class.
         //Also, at some point the functionality to add a profile picture might need to be added to this method.
-        $sql = "INSERT INTO userprofile (Username, UserPassword) VALUES ('$Username', '$UserPassword')";
-        
-        $statement = $this->con->getPDO()->prepare($sql); 
-        $statement->execute();
+        $sqlQuery = 'INSERT INTO userprofile (Username, UserPassword) VALUES (:username, :userPassword)';
+        $statement = $this->con->getPDO()->prepare($sqlQuery); 
+        $statement->execute([ 'username' => $username, 'userPassword' => $userPassword ]);
     }
 }
 
