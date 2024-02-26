a6d4f0fd38d8d9a77ca3d4d6c7cc2a888a12ad91
jonasplate <plate_91@hotmail.com>
2024-02-26 11:17:28 +0100

    library connection showinfo

--- library.php
+++ library.php
@@ -40,27 +40,25 @@
   <!-- Innehåll på sidan -->
   <main>
     
-
+<div class="container">
   <?php $con = new Connection();
 $songdao = new SongDAO($con); // Notera att $con nu korrekt passas till konstruktören
 
 $songs = $songdao->findAll();
-foreach ($songs as $song) {
+// foreach ($songs as $song) {
     // Anta att $song->title är en publik egenskap i din Song-klass
-    echo "<div><button><p>Artist: " . htmlspecialchars($song->Artist) . ($song->Rating) . "</p></button></div>";
-    echo "<div><p>Genre: " . htmlspecialchars($song->Genre) . "</p></div>";
+   // echo "<div><button><p>Artist: " . htmlspecialchars($song->Artist) . ($song->AverageRating) . "</p></button></div>";
+  //  echo "<div><p>Genre: " . htmlspecialchars($song->Genre) . "</p></div>";
+// }
+ // echo "<div><a href='player.php?artist=" . urlencode($song->Artist) . "&rating=" . urlencode($song->AverageRating) . "&genre=" . urlencode($song->Genre) . "' class='button'>Artist: " . htmlspecialchars($song->Artist) . " - Rating: " . htmlspecialchars($song->AverageRating) . " - Genre: " . htmlspecialchars($song->Genre) . "</a></div>";
+
+
+foreach ($songs as $song) {
+    echo "<div><a href='player.php?artist=" . urlencode($song->Artist) . "&rating=" . urlencode($song->AverageRating) . "&genre=" . urlencode($song->Genre) . "' class='button'>Artist: " . htmlspecialchars($song->Artist) . " - Rating: " . htmlspecialchars($song->AverageRating) . " - Genre: " . htmlspecialchars($song->Genre) . "</a></div>";
 }
+
 ?>
-
-<?php foreach ($numbers as $number): ?>
-   <div class="container"><button><?= htmlspecialchars($number) ?></button></div>
-<?php endforeach; ?>
-
-    <!-- <div class="container">
-      ?php foreach ($fruits as $fruit): ?>
-        <button>?php echo $fruit; ?></button>
-      ?php endforeach; ?>
-    </div> -->
+</div>
 
   </main>
 </body>
